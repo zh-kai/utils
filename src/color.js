7dd0f2fb504ef2e41ae07dc23291a3bbc7df985f
zh-kai <zh_kai0531@163.com>
2023-02-12 15:58:44 +0800

    add: hex2rgb

---
+++ src/color.js
@@ -0,0 +1,15 @@
+/**
+ * 十六进制颜色 -> rgb
+ * @param {*} hex #xxxxxx
+ * @returns {Object} {r, g, b}
+ */
+const hex2rgb = (hex) => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
